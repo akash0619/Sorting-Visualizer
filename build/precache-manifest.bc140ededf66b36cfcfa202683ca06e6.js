f356ed131321b185ed8d7d2d2ef8bbf4dd66409e
akashz19 <anand.btech.cs18@iiitranchi.ac.in>
2021-07-26 21:13:47 +0530

    Changes

---
+++ build/precache-manifest.bc140ededf66b36cfcfa202683ca06e6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9c160c513da7ba9a51dc3e2d6f87fe31",
+    "url": "/Sorting-Visualizer/index.html"
+  },
+  {
+    "revision": "80a504162213c8bb752b",
+    "url": "/Sorting-Visualizer/static/css/2.510409cd.chunk.css"
+  },
+  {
+    "revision": "80a504162213c8bb752b",
+    "url": "/Sorting-Visualizer/static/js/2.fe80d79f.chunk.js"
+  },
+  {
+    "revision": "17cfa68310281a2ba096",
+    "url": "/Sorting-Visualizer/static/js/main.99df22ec.chunk.js"
+  },
+  {
+    "revision": "bb456b206216dd26c6ed",
+    "url": "/Sorting-Visualizer/static/js/runtime-main.7e78c47a.js"
+  }
+]);
--- src/Navigation.js
+++ src/Navigation.js
@@ -4,7 +4,7 @@ class Navigation extends Component {
   render() {
     return (
       <nav className="navbar navbar-expand-sm bg-primary navbar-dark">
-        <a className="navbar-brand" href="/">
+        <a className="navbar-brand" href="https://akashz19.github.io/Sorting-Visualizer/">
           Sorting Visualizer
         </a>
         <div className="navbar-nav ml-auto">
